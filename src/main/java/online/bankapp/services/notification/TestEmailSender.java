1fa1758145a617ba0e23d40b88c1f34a547484e8
Pawel-Mackiewicz <pawel@mackiewicz.info>
2025-10-01 18:53:54 +0200

    refactor: replace Runner with TestEmailSender and introduce AppProperties configuration

--- src/main/java/online/bankapp/services/notification/TestEmailSender.java
+++ src/main/java/online/bankapp/services/notification/TestEmailSender.java
@@ -2,27 +2,33 @@ package online.bankapp.services.notification;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import online.bankapp.services.notification.config.AppProperties;
 import online.bankapp.services.notification.config.WelcomeMessagePubSubConfig;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBooleanProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 import java.util.UUID;
 
+@ConditionalOnBooleanProperty(
+        name = "app.send-test-email"
+)
 @Slf4j
 @Component
 @RequiredArgsConstructor
+@EnableConfigurationProperties(AppProperties.class)
+public class TestEmailSender implements CommandLineRunner {
 
-//for testing purposes
-//delete this class for prod
-public class Runner implements CommandLineRunner {
-
-    String EMAIL_TO_SEND_TEST_MSG = "testEmail@mackiewicz.info";
+    private final AppProperties appProperties;
 
     private final RabbitTemplate rabbitTemplate;
 
     @Override
     public void run(String... args) {
+
+        String EMAIL_TO_SEND_TEST_MSG = appProperties.testEmailAddress();
 
         log.info("Sending test msg");
         UUID userUUID = UUID.randomUUID();
---
+++ src/main/java/online/bankapp/services/notification/config/AppProperties.java
@@ -0,0 +1,7 @@
+package online.bankapp.services.notification.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "app")
+public record AppProperties(String testEmailAddress) {
+}
